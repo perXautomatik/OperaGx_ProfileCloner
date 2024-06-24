0fe6227edb764f3114cdf824b25200c71364ca98
perXautomatik <christoffer.broback@gmail.com>
2024-06-24 17:15:45 +0200

    seems stable, successfully refractored clearCache

--- man.ps1
+++ man.ps1
@@ -41,6 +41,7 @@ Begin {
 	Push-Location 
 	cd $DriveLetter
 	$global:setFilextPath = (Resolve-Path -Relative -path "./Set-FileExtensions.ps1");
+	$global:ClearCachePath = (Resolve-Path -Relative -path "./Clear-Cache.ps1");
 	Pop-Location
 
 	$Default = @{
@@ -87,205 +88,33 @@ Begin {
 
 			return $AllArgs
 	}
+
+	
+	function HashKeys-ByFunction {
+		param (
+			$commandName,
+			$HashedParams
+			)
+		# Get the list of valid parameter names for the function
+        $validParameters = (Get-Command $commandName).Parameters.Keys
+        # Filter out invalid parameters
+        $setFilext = $HashedParams.Keys | ? { $_ -notin $validParameters } ;
+        $setFilext | % { $HashedParams.Remove($_) }
+        
+        return $HashedParams						
+    }
 	
 	# Prepare launcher options
 	$OperaLaunchParams = @{
 		FilePath = $launcher
 		ArgumentList = (Prepare-LauncherOptions -inputParams $launchParams)
 	}
-
-	function Process-WithProgressBar {
-		[CmdletBinding()]
-		param (
-			[parameter(ValueFromPipeline)]
-			$_,
-			[Parameter(Mandatory = $true)]
-			[scriptblock]$ProcessBlock,
-			[Parameter(Mandatory = $true)]
-			[hashtable]$ProgressParams
-		)
-
-		begin {
-			# Initialize variables that are used in the process block
-			$current = 0
-			$total = $ProgressParams.TotalCount
-			$shell = $Host.UI.RawUI
-		}
-
-		process {
-			# Process the current piped object
-			$currentObject = $_
-
-			# Increment the current count
-			$current++
-
-			# Calculate the percentage of completion
-			$percent = ($current / $total) * 100
-
-			# Update the progress parameters
-			$ProgressParams.PercentComplete = $percent
-			$ProgressParams.Status = "Processing item $current of $total"
-			
-			#invoke
-			$ProgressParams.CurrentOperation = $ProcessBlock.Invoke($currentObject).ToString()
-			$ProgressParams = HashKeys-ByFunction Write-Progress $ProgressParams;						
-			# Display the progress bar using splatting
-			Write-Progress @ProgressParams
-
-			# Update the console title
-			$shell.WindowTitle = "Progress $($ProgressParams.PercentComplete)%"
-		}
-
-		end {
-			# Any cleanup code if needed
-		}
-	}
-
-
-	function HashKeys-ByFunction {
-		param (
-			$commandName,
-			$HashedParams
-		)
-			# Get the list of valid parameter names for the function
-			$validParameters = (Get-Command $commandName).Parameters.Keys
-			# Filter out invalid parameters
-			$setFilext = $HashedParams.Keys | ? { $_ -notin $validParameters } ;
-			$setFilext | % { $HashedParams.Remove($_) }
-
-			return $HashedParams						
-		}
-
-	function Clear-Cache {
-		[CmdletBinding()]
-		param (
-			$params,
-			$driveLet,
-			$PathSufix,
-			$SourceFolder,
-			$ChildNode,
-			$ProfileFolderPath
-		 )
-		begin 		
-		{
-		
-			$PreparedParams = @{
-				SessionStorage = "$($params.DriveLet)\sessionStorage"
-				SourceFolder = (Join-Path -Path $params.DriveLet -ChildPath $params.PathSufix)
-				ProfileLocation = ""
-			}
-
-			$PreparedParams.ProfileLocation = 
-			Join-Path -Path $PreparedParams.SourceFolder -ChildPath $params.ChildNode
-
-			function Get-SessionId {
-				param(
-					$childPath
-				)
-		
-				$internalItems = ($childPath | get-childitem );
-				$firtFile = (($internalItems | Sort-Object CreationTime | Select-Object -First 1).CreationTime);
-				$lastFile = (($internalItems | Sort-Object CreationTime -Descending | Select-Object -First 1).CreationTime);
-				$setFilext = $lastFile -$firtFile
-		
-				if($setFilext.Days -gt 0)
-				{
-					$from = get-date -date $firtFile  -Format "yyMMdd_HHmmss"
-					$to = get-date -date $lastFile  -Format "yyMMdd_HHmmss"
-				}
-		
-				$sessionId = (Get-Date -Format "yyMMdd_HHmmss");
-				return $sessionId;
-			}
-
-			function Move-BasedOnExtension {
-				[CmdletBinding()]
-				param (
-					[Parameter(Mandatory = $true)] [string]$OriginalFolderPath,			
-					[Parameter(Mandatory = $true)] [string]$ExcludedExtensions,			
-					[Parameter(Mandatory = $true)] [string]$NewFolderPath
-				)
-							
-				# Retrieve all files in the original folder path
-				$files = Get-ChildItem -Path $OriginalFolderPath -File
-								
-				# Filter files to exclude the specified extensions
-				$filesToMove = $files | Where-Object { $_.Extension -notin $ExcludedExtensions -split "," }
-			
-				# Check if there are files to move
-				if ($filesToMove) {
-					# Ensure the new folder path exists
-					$null = New-Item -ItemType Directory -Force -Path $NewFolderPath
-					if (Test-Path $NewFolderPath) {
-						# Move the filtered files
-						foreach ($file in $filesToMove) {
-							Move-Item -Path $file.FullName -Destination $NewFolderPath -PassThru
-						}
-				
-						# Output the count of moved files
-						Write-Verbose "$($filesToMove.Count) file(s) moved to $NewFolderPath"	
-					}
-					else {
-						Write-Error "SessionFolder not created!"
-						exit
-					}
-					
-				}
-				else {
-					Write-Host "No files to move based on the specified extensions."
-				}
-			}
-			
-
-			# Navigate to the source folder
-			#Push-Location
-			#Set-Location -Path $SourceFolder
-			# Retrieve cache directories and process each one
-			$toProcess = Get-ChildItem -Path $PreparedParams.ProfileLocation -Depth 1 -Include "cache" | % {Get-ChildItem -Path $_.FullName  }		
-		}
-		process {
-			$toProcess  | %{
-				$currentFolder = $_
-				$parentName = $currentFolder.Parent.Name
-				$newFolder = Join-Path -Path $PreparedParams.SessionStorage -ChildPath (join-path $parentName (Get-SessionId -ChildPath $currentFolder))
-			
-				if ((Get-ChildItem $currentFolder -ErrorAction SilentlyContinue).Length -gt 0) {
-					# Check if the new folder already exists
-					if ((Get-ChildItem -Path $newFolder -ErrorAction SilentlyContinue).Length -gt 0) {
-						Write-Debug "The folder already exists."
-					} else {
-						
-						# Change file extensions before moving
-						$currentFolder.FullName | & $global:setFilextPath
-
-						# Define parameters for moving files based on extension
-						$moveParams = @{
-							ExcludedExtension = $ExcludedExtensions
-							OriginalFolderPath = $currentFolder.FullName
-							NewFolderPath = $newFolder
-						}
-						
-						# Move files based on the defined parameters
-						Move-BasedOnExtension @moveParams
-					}	
-				}
-				else {
-					Write-Debug "the folder is empty, skipping"
-				}
-				
-			}
-		}
-
-		end {
-			# Return to the original location
-			Pop-Location	
-		}
-	}
-
+	
 }
 
 # Process block
 Process {
+	. $global:ClearCachePath
 	Clear-Cache (HashKeys-ByFunction "Clear-Cache" $CacheClearParams)
     
 	Write-Verbose "Invoking OperaLauncher with parameter $childNode on drive $driveLetter"
